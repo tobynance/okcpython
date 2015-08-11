3e68f70e2fc122571d272b09058755af31261d56
Toby Nance <toby.nance@draft2digital.com>
2015-08-10 19:50:23 -0500

    Implemented tk human client

---
+++ contests/2015_10_14_battleship/client/human/human_tk/human.py
@@ -0,0 +1,63 @@
+import sys
+from Tkinter import *
+import thread
+
+
+########################################################################
+class HumanClient(object):
+    ####################################################################
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Battleship")
+
+        top_frame = Frame(self.root)
+        top_frame.pack(side=TOP, expand=True, fill=BOTH)
+
+        self.scrollbar = Scrollbar(top_frame)
+        self.list_box = Listbox(top_frame, height=24, width=100)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.list_box.pack(side=LEFT, expand=True, fill=BOTH)
+        self.scrollbar.config(command=self.list_box.yview)
+        self.list_box.config(yscrollcommand=self.scrollbar.set)
+
+        bottom_frame = Frame(self.root)
+        bottom_frame.pack(side=BOTTOM, fill=X)
+        Label(bottom_frame, text="User Input").pack(side=LEFT)
+        self.text_entry = Entry(bottom_frame)
+        self.text_entry.insert(END, "")
+        self.text_entry.pack(side=RIGHT, expand=True, fill=X)
+        self.text_entry.bind("<Return>", lambda event: self.send_response())
+        self.text_entry.bind("<KP_Enter>", lambda event: self.send_response())
+
+    ####################################################################
+    def run(self):
+        mainloop()
+
+    ####################################################################
+    def send_response(self):
+        text = self.text_entry.get()
+        print text.strip()  # send it to standard out, followed by a newline
+        self.text_entry.delete(0, END)
+
+    ####################################################################
+    def add_line(self, line):
+        self.list_box.insert(END, line)
+        self.list_box.select_clear(self.list_box.size() - 2)  # Clear the current selected item
+        self.list_box.select_set(END)                         # Select the new item
+        self.list_box.yview(END)                              # Set the scrollbar to the end of the list_box
+
+
+########################################################################
+def run_listener(client):
+    while True:
+        line = sys.stdin.readline()
+        line = line.strip()
+        if line:
+            client.add_line(line)
+
+
+########################################################################
+if __name__ == "__main__":
+    self = HumanClient()
+    thread.start_new_thread(run_listener, (self,))
+    self.run()
--- contests/2015_10_14_battleship/server/players.py
+++ contests/2015_10_14_battleship/server/players.py
@@ -1,6 +1,7 @@
 import subprocess
 import server
 
+
 #######################################################################
 class Player(object):
     def __init__(self, name, popen_args, cwd):
@@ -13,10 +14,11 @@ class Player(object):
         s = subprocess.Popen(self.popen_args + [self.name, other.name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.cwd)
         return server.ClientProxy(self.name, s)
 
+
 #######################################################################
 def getPlayers():
     all_players = {}
-    all_players["human"] = Player("Human", ["python", "human_client.py"], "../client/human")
+    all_players["human"] = Player("Human", ["python", "human.py"], "../client/human_tk")
     all_players["basic1"] = Player("Basic1", ["python", "simple_client.py"], "../client/python_sample")
     all_players["basic2"] = Player("Basic2", ["python", "simple_client.py"], "../client/python_sample")
 
